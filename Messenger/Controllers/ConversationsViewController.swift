552dffc7ca8311fd94a4ddf6ade568841bcc9d40
Ma'el Hashemee <ahashemee.22@acsamman.edu.jo>
2021-05-07 23:19:55 +0300

    Initial Commit

---
+++ Messenger/Controllers/ConversationsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Ma'el Hashemee on 5/7/21.
+//
+
+import UIKit
+/* I've changed the name from ViewController to ConversationsViewController as it becomes
+ more descriptive and more indicative of what it will be used for in the future. */
+ class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
